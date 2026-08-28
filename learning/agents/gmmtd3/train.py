41261b3ab6cad7ea021928f8c1fc7a8c510575c8
khsquant <141811494+khsquant@users.noreply.github.com>
2026-08-28 17:27:36 +0900

    Update train.py

--- learning/agents/gmmtd3/train.py
+++ learning/agents/gmmtd3/train.py
@@ -335,7 +335,7 @@ def train(
   make_policy = gmmtd3_networks.make_inference_fn(gmmtd3_network)
   N_REF = min(64, num_envs)
   ref_rng = jax.random.split(jax.random.PRNGKey(0), num_envs)
-  ref_obs = env.reset(ref_rng).obs[:N_REF]
+  ref_obs = env.reset(ref_rng).obs["state"][:N_REF]
 
   policy_optimizer = optax.adam(learning_rate=learning_rate)
   q_optimizer = optax.adam(learning_rate=learning_rate)    
